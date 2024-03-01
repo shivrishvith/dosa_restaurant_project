a7dc822b1d8294a4c73e3e6088f032afb30d65da
Shivrishvith <shivrishvith@gmail.com>
2024-03-01 01:55:37 -0500

    Added script to create customers json file

--- process_orders.py
+++ process_orders.py
@@ -3,3 +3,8 @@ import json
 def read_orders(file_name):
     with open(file_name, 'r') as file:
         return json.load(file)
+
+def create_customers_json(orders):
+    customers = {order['customer']['phone']: order['customer']['name'] for order in orders}
+    with open('customers.json', 'w') as file:
+        json.dump(customers, file)
