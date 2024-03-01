4fa39068dea357d459747b42bf68be9ca88d7cea
Shivrishvith <shivrishvith@gmail.com>
2024-03-01 01:57:15 -0500

    Added script to process orders from JSON

--- process_orders.py
+++ process_orders.py
@@ -8,3 +8,19 @@ def create_customers_json(orders):
     customers = {order['customer']['phone']: order['customer']['name'] for order in orders}
     with open('customers.json', 'w') as file:
         json.dump(customers, file)
+
+def create_items_json(orders):
+    items = {}
+    for order in orders:
+        for item in order['items']:
+            if item['name'] not in items:
+                items[item['name']] = {'price': item['price'], 'orders': 1}
+            else:
+                items[item['name']]['orders'] += 1
+    with open('items.json', 'w') as file:
+        json.dump(items, file)
+
+if __name__ == '__main__':
+    orders_data = read_orders('example_orders.json')
+    create_customers_json(orders_data)
+    create_items_json(orders_data)
