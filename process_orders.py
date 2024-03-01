7ad7340c29e0aafcfab08173aafcd51ba34d554f
Shivrishvith <shivrishvith@gmail.com>
2024-03-01 10:44:23 -0500

    made changes to the script as needed

--- process_orders.py
+++ process_orders.py
@@ -5,7 +5,11 @@ def read_orders(file_name):
         return json.load(file)
 
 def create_customers_json(orders):
-    customers = {order['customer']['phone']: order['customer']['name'] for order in orders}
+    customers = {}
+    for order in orders:
+        phone = order['phone']
+        name = order['name']
+        customers[phone] = name
     with open('customers.json', 'w') as file:
         json.dump(customers, file)
 
@@ -13,14 +17,16 @@ def create_items_json(orders):
     items = {}
     for order in orders:
         for item in order['items']:
-            if item['name'] not in items:
-                items[item['name']] = {'price': item['price'], 'orders': 1}
+            name = item['name']
+            price = item['price']
+            if name not in items:
+                items[name] = {'price': price, 'orders': 1}
             else:
-                items[item['name']]['orders'] += 1
+                items[name]['orders'] += 1
     with open('items.json', 'w') as file:
         json.dump(items, file)
 
 if __name__ == '__main__':
-    orders_data = read_orders('example_orders.json')
+    orders_data = read_orders('example_orders.json')  # Ensure this path is correct
     create_customers_json(orders_data)
     create_items_json(orders_data)
